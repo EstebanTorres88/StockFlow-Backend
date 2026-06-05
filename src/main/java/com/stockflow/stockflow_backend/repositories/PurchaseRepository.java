59ab6c047162b7c41a1320d8ec4b9be5adb5933a
Angely Castro <angely.castro@ucr.ac.cr>
2026-06-05 09:55:55 -0600

    feat: add PurchaseService

--- src/main/java/com/stockflow/stockflow_backend/repositories/PurchaseRepository.java
+++ src/main/java/com/stockflow/stockflow_backend/repositories/PurchaseRepository.java
@@ -1,5 +1,8 @@
 package com.stockflow.stockflow_backend.repositories;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.stockflow.stockflow_backend.entities.Purchase;
@@ -7,5 +10,6 @@ import com.stockflow.stockflow_backend.entities.Purchase;
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
     //los metodos los hereda de JpaRepository, no es necesario definirlos aqui
+    Optional<Purchase> findByResourceId(UUID resourceId);
     
 }
